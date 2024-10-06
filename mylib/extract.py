7fb0e73ebf9d1a240494cb6e4b6b11d706eda5a2
Kaisen Yao <kaisenyao@MacBookPro.lan>
2024-10-05 21:22:16 -0400

    test

--- mylib/extract.py
+++ mylib/extract.py
@@ -2,7 +2,7 @@ import requests
 
 
 def extract(
-    url="https://reurl.cc/pvAYlx",
+    url="https://raw.githubusercontent.com/fivethirtyeight/data/refs/heads/master/births/US_births_2000-2014_SSA.csv",
     file_path="data/US_births.csv",
 ):
     """ "Extract a url to a file path"""
