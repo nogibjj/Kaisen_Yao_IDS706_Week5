88efbe1cc65a476adb92d21754b7e89366722d49
Kaisen Yao <kaisenyao@MacBookPro.lan>
2024-10-05 21:13:35 -0400

    test

--- mylib/extract.py
+++ mylib/extract.py
@@ -12,7 +12,7 @@ def extract(
     "refs/heads/master/births/US_births_2000-2014_SSA.csv",
     file_path="data/US_births_2000-2014_SSA.csv",
 ):
-    """ "Extract a url to a file path"""
+    """Extract a URL to a file path."""
     with requests.get(url) as r:
         with open(file_path, "wb") as f:
             f.write(r.content)
